0db83b293a03b009027e7017d2a40e97a163ef87
KHSCS002 <KHSCS002@KATYISD.ORG>
2019-02-07 09:52:46 -0600

    Combining

--- src/org/settlersofcatan/Vertex.java
+++ src/org/settlersofcatan/Vertex.java
@@ -1,12 +1,42 @@
 package org.settlersofcatan;
 
 import javafx.scene.control.Button;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import javafx.scene.image.ImageView;
+
+import static java.lang.System.*;
+import java.awt.Color;
+import java.io.*;
+import java.lang.Math;
 
 public class Vertex extends Button
 {
 	private int gridCol, gridRow;
 	private boolean hasBuilding, exists;
 	private Building building; 
+	int vertexID;
+	int tileID;
+	
+	public Vertex()
+	{
+		vertexID = 0;
+		tileID = 0;
+	}
+	
+	public Vertex(int x, int y)
+	{
+		vertexID = x;
+		tileID = y;
+	}
+	
+	public Vertex(Vertex v)
+	{
+		vertexID = v.vertexID;
+		tileID = v.tileID;
+	}
 	
 	public Vertex(int row, int col, boolean exists) 
 	{
@@ -54,6 +84,11 @@ public class Vertex extends Button
 	public void setBuilding(Building building) 
 	{
 		this.building = building;
+	}
+	
+	public String toString()
+	{
+		return tileID+", "+tileID;
 	}
 	
 }
